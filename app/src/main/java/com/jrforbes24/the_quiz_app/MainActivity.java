cd14917e2c34ba0d7590bf695bf368345867cddd
urock24 <jrforbes24@gmail.com>
2018-02-28 21:35:52 -0700

    added code to check for done on the EditText view

--- app/src/main/java/com/jrforbes24/the_quiz_app/MainActivity.java
+++ app/src/main/java/com/jrforbes24/the_quiz_app/MainActivity.java
@@ -1,13 +1,55 @@
 package com.jrforbes24.the_quiz_app;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+//    this is an EditText variable theName that will hold the EditText state from the xml
+    private EditText theName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Pulling in the_name EditText and assigning it the theName
+        theName = (EditText) findViewById(R.id.the_name);
+
+        theName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((actionId == EditorInfo.IME_ACTION_DONE) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN ))){
+                    String  myName = theName.getText().toString();
+                    Context context = getApplicationContext();
+                    CharSequence text = "Hello " + myName;
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+                    return true;
+                }
+                else {
+                    return false;
+                }
+
+            }
+        });
+
     }
+
 }
+
+
+
