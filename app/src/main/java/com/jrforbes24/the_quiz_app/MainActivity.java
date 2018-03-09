a42825fb4757828e41c709f1eb464ed5604d695d
urock24 <jrforbes24@gmail.com>
2018-03-08 19:25:08 -0700

    more work on start button and hiding views

--- app/src/main/java/com/jrforbes24/the_quiz_app/MainActivity.java
+++ app/src/main/java/com/jrforbes24/the_quiz_app/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,15 +24,17 @@ public class MainActivity extends AppCompatActivity {
     // integer to hold the total possible
     int possibleScore = 8;
     //    question number that will be iterated
-    int qustionNumber = 0;
+    int questionNumber = 0;
+    // variable to store the correct answer to check against for the answer.
+    String correctAnswer = "";
     //    array to store the questions
     String[] questionArray = {
             "What is the most popular pistol caliber?",
             "How amy wars did Colonel Potter from MASH fight in?",
             "When is Justin Bieber's birthday?",
             "What is the best rock band of all time?",
-            "Who wrote 'The Three Musketeers'",
-            "Who wrote the line 'And miles to go before I sleep'",
+            "Who wrote 'The Three Musketeers'?",
+            "Who wrote the line 'And miles to go before I sleep'?",
             "What was the name of the probe that flew by Pluto in July 2015?",
             "What is the name of the closest star to Earth?"
     };
@@ -55,14 +59,15 @@ public class MainActivity extends AppCompatActivity {
 //        Pulling in the_name EditText and assigning it the theName
         theName = (EditText) findViewById(R.id.the_name);
 
-
+        /*
+        * Method to listen for actions on theName text field
+        */
         theName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((actionId == EditorInfo.IME_ACTION_DONE) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN))) {
                     hideKeyboard();
                     createHelloToast();
-
                     return true;
                 } else {
                     return false;
@@ -120,8 +125,16 @@ public class MainActivity extends AppCompatActivity {
     * also sets text of button to QUIT
     */
     public void startQuiz(View view) {
+//      method to populate the question text field.
+        populateQuestion(view);
+        populateAnswer(view);
+//        this will set the Text view to visible
+        TextView theQuestion = findViewById(R.id.question_TV);
+        theQuestion.setVisibility(View.VISIBLE);
+//        change the start button to quit
         Button whichButton = findViewById(R.id.whichButton);
         whichButton.setText(getString(R.string.quit_button));
+
     }
 
     /*
@@ -129,9 +142,96 @@ public class MainActivity extends AppCompatActivity {
     * and reset anything else I can't think of right now back to the starting position.
     */
     public void quitQuiz(View view) {
+//        set the global variables back to default
+        theScore = 0;
+        questionNumber = 0;
+        correctAnswer = "";
+
+//        hide the question view and answer views
+        TextView theQuestion = findViewById(R.id.question_TV);
+        theQuestion.setVisibility(View.GONE);
+        EditText text_answer = findViewById(R.id.the_answer_text);
+        text_answer.setVisibility(View.GONE);
+        RadioGroup radio_group = findViewById(R.id.multi_radio);
+        radio_group.setVisibility(View.GONE);
+        LinearLayout the_check_boxs = findViewById(R.id.check_box);
+        the_check_boxs.setVisibility(View.GONE);
+
+//        change the quit button to start
         Button whichButton = findViewById(R.id.whichButton);
         whichButton.setText(getString(R.string.start_button));
     }
+
+    /*
+    * populate question gets question from array and sets the question in the question_TV
+    */
+    public void populateQuestion(View view) {
+        TextView theQuestion = findViewById(R.id.question_TV);
+        theQuestion.setText(getString(R.string.the_question, questionArray[questionNumber]));
+    }
+
+    /*
+    * populateAnswer gets the answer from the array and based on the array will change
+    * the answer view and populate the appropriate fields.
+    */
+    public void populateAnswer(View view) {
+//        find out which view to show and which view to hide.
+        String theView = answerArray[questionNumber][0];
+        if(theView.equals("radio")) {
+            populateRadioButtons(view);
+            RadioGroup radio_group = findViewById(R.id.multi_radio);
+            radio_group.setVisibility(View.VISIBLE);
+            EditText text_answer = findViewById(R.id.the_answer_text);
+            text_answer.setVisibility(View.GONE);
+            LinearLayout the_check_boxs = findViewById(R.id.check_box);
+            the_check_boxs.setVisibility(View.GONE);
+        }
+        else if(theView.equals("text")) {
+            populateTextField(view);
+            RadioGroup radio_group = findViewById(R.id.multi_radio);
+            radio_group.setVisibility(View.GONE);
+            EditText text_answer = findViewById(R.id.the_answer_text);
+            text_answer.setVisibility(View.VISIBLE);
+            LinearLayout the_check_boxs = findViewById(R.id.check_box);
+            the_check_boxs.setVisibility(View.GONE);
+        }
+        else {
+            populateCheckBoxes(view);
+            RadioGroup radio_group = findViewById(R.id.multi_radio);
+            radio_group.setVisibility(View.GONE);
+            EditText text_answer = findViewById(R.id.the_answer_text);
+            text_answer.setVisibility(View.GONE);
+            LinearLayout the_check_boxs = findViewById(R.id.check_box);
+            the_check_boxs.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /*
+    * populate the strings for each radio button
+    * call method for listener on the check boxes
+    */
+    public void populateRadioButtons(View view) {
+//        TODO
+    }
+
+    /*
+    * populate the hint for the text field
+    * call the method to check for the keyboard done
+    */
+    public void populateTextField(View view) {
+//        TODO
+    }
+
+    /*
+    * populate the check boxes and set listener
+    */
+    public void populateCheckBoxes(View view) {
+//        TODO
+    }
+
+
 }
+
+
 
 
